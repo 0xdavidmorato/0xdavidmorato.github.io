58cc6382abb31d0b23e4e03e32ce77b695e4b7dd
David Morato <86056036+0xdavidmorato@users.noreply.github.com>
2024-04-30 11:45:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,33 +6,36 @@ document.addEventListener("DOMContentLoaded", function () {
   let count = 0;
 
   function createBubbles() {
+    // Document fragment
+    const fragment = document.createDocumentFragment();
     for (let i = 0; i < 20; i++) {
       const bubble = document.createElement("div");
       bubble.classList.add("bubble");
       const pos = getRandomPos();
       const size = getRandomSize(25, 150);
 
-      bubble.style.left = pos.x + "px";
-      bubble.style.top = pos.y + "px";
-      bubble.style.width = size + "px";
-      bubble.style.height = size + "px";
+      // Template literals
+      bubble.style.cssText = `left:${pos.x}px; top:${pos.y}px; width:${size}px; height:${size}px;`;
 
-      bubbleContainer.appendChild(bubble);
+      fragment.appendChild(bubble);
     }
+    bubbleContainer.appendChild(fragment);
   }
 
-bubbleContainer.addEventListener("click", function (e) {
-  if (e.target.classList.contains("bubble")) {
-    const bubble = e.target;
-    bubble.classList.add("burst");
+  // click events
+  bubbleContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("bubble")) {
+      const bubble = e.target;
+      bubble.classList.add("burst");
 
-    setTimeout(() => {
-      bubble.remove();
-    }, 300);
-    count++;
-    counterDisplay.textContent = count;
-  }
-});
+      // wait until burst effect finished
+      setTimeout(() => {
+        bubble.remove();
+      }, 300);
+      count++;
+      counterDisplay.textContent = count;
+    }
+  });
 
   resetBtn.addEventListener("click", function () {
     bubbleContainer.innerHTML = "";
@@ -42,10 +45,14 @@ bubbleContainer.addEventListener("click", function (e) {
   });
 
   function getRandomPos() {
-  const x = Math.floor(Math.random() * (window.innerWidth - 100));
-  const y = Math.floor(Math.random() * (window.innerHeight - 100));
-  return { x, y };
-}
+    const x = Math.floor(
+      Math.random() * (document.documentElement.clientWidth - 100)
+    );
+    const y = Math.floor(
+      Math.random() * (document.documentElement.clientHeight - 100)
+    );
+    return { x, y };
+  }
 
   function getRandomSize(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
