99d8fee66b2004a59111ff55049fa9553e66c418
David Morato <86056036+0xdavidmorato@users.noreply.github.com>
2024-08-04 23:44:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,82 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const counterDisplay = document.querySelector(".counter");
-  const resetBtn = document.querySelector(".reset");
-  const bubbleContainer = document.querySelector(".bubble-container");
+const iconBoxes = document.querySelectorAll(".icon-box");
+const iconBoxContainers = document.querySelectorAll(".icon-container");
+const closeBtns = document.querySelectorAll(".close-btn");
+const maximizeBtns = document.querySelectorAll(".maximize-btn");
+const body = document.querySelector("body");
 
-  let count = 0;
-
-  function createBubbles() {
-    // Document fragment
-    const fragment = document.createDocumentFragment();
-    for (let i = 0; i < 20; i++) {
-      const bubble = document.createElement("div");
-      bubble.classList.add("bubble");
-      const pos = getRandomPos();
-      const size = getRandomSize(25, 150);
-
-      // Template literals
-      bubble.style.cssText = `left:${pos.x}px; top:${pos.y}px; width:${size}px; height:${size}px;`;
-
-      fragment.appendChild(bubble);
-    }
-    bubbleContainer.appendChild(fragment);
-  }
-
-  // click events
-  bubbleContainer.addEventListener("click", function (e) {
-    if (e.target.classList.contains("bubble")) {
-      const bubble = e.target;
-      bubble.classList.add("burst");
-
-      // wait until burst effect finished
-      setTimeout(() => {
-        bubble.remove();
-      }, 300);
-      count++;
-      counterDisplay.textContent = count;
-    }
+iconBoxes.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let modal = btn.getAttribute("data-modal");
+    document.getElementById(modal).style.display = "block";
+    body.classList.add("prevent-background-scroll");
   });
+});
 
-  resetBtn.addEventListener("click", function () {
-    bubbleContainer.innerHTML = "";
-    count = 0;
-    counterDisplay.textContent = count;
-    createBubbles();
+closeBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let modal = btn.closest(".popup");
+    modal.style.display = "none";
+    body.classList.remove("prevent-background-scroll");
+    iconBoxContainers.forEach((container) => {
+      container.style.display = "flex";
+    });
   });
+});
 
-  function getRandomPos() {
-    const x = Math.floor(
-      Math.random() * (document.documentElement.clientWidth - 100)
-    );
-    const y = Math.floor(
-      Math.random() * (document.documentElement.clientHeight - 100)
-    );
-    return { x, y };
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("popup")) {
+    e.target.style.display = "none";
+    body.classList.remove("prevent-background-scroll");
   }
+});
 
-  function getRandomSize(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
+maximizeBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let modal = btn.closest(".popup");
+    let container = modal.querySelector(".popup-container");
+    let body = modal.querySelector(".popup-body");
 
-  createBubbles();
+    if (modal.classList.contains("maximized")) {
+      container.style.width = "min(900px, 90%)";
+      container.style.top = "45%";
+      body.style.height = "70vh";
+    } else {
+      container.style.width = "100%";
+      container.style.top = "50%";
+      body.style.height = "90vh";
+    }
+
+    modal.classList.toggle("maximized");
+    body.classList.toggle("prevent-scroll");
+  });
+});
+
+var swiper = new Swiper(".swiper", {
+  preventClicks: true,
+  noSwiping: true,
+  freeMode: false,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+  mousewheel: {
+    invert: false,
+    thresholdDelta: 50,
+    sensitivity: 1,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    680: {
+      slidesPerView: 2,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    1600: {
+      slidesPerView: 4,
+    },
+  },
 });
